0585e19f7d2f728c40177b23b3f649b52769f713
boolfalse <boolfalse@mail.ru>
2021-11-22 14:51:15 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,68 @@
+
+const { parse: index } = require('node-html-parser');
+
+fs = require('fs')
+fs.readFile('./sider.html', 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    const root = index(data);
+
+    let menu = [];
+    const categories = root.querySelectorAll('.level-0'); // .level-0
+
+    categories.forEach((category, k) => {
+        let categoryImg = category.querySelector('a.level-0-link img');
+        let menuItem = {
+            name: categoryImg.attributes.alt,
+            image: categoryImg.attributes.src,
+        };
+
+        let menuSubItems = [];
+        // .level-0 .level-1-container .level-1
+        let subcategories = category.querySelectorAll('.level-1-container .level-1');
+
+        subcategories.forEach((subcategory, i) => {
+            if (i > 0) {
+                let subcategoryImg = subcategory.querySelector('a.level-1-link img');
+                let menuSubItem = {
+                    name: subcategoryImg.attributes.alt,
+                    image: subcategoryImg.attributes.src,
+                };
+
+                let menuSubSubItems = [];
+                // .level-0 .level-1-container .level-1 .level-2-container .level-2
+                let subsubcategories = subcategory.querySelectorAll('.level-2-container .level-2');
+                subsubcategories.forEach((subsubcategory, j) => {
+                    if (j > 0) {
+                        let subsubcategoryImg = subsubcategory.querySelector('a.level-2-link img');
+                        if (subsubcategoryImg) {
+                            menuSubSubItems.push({
+                                name: subsubcategoryImg.attributes.alt,
+                                image: subsubcategoryImg.attributes.src,
+                            });
+                        }
+                    }
+                });
+
+                menuSubItem.children = menuSubSubItems;
+                menuSubItems.push(menuSubItem);
+            }
+        });
+
+        menuItem.children = menuSubItems;
+        menu.push(menuItem);
+    });
+
+    fs.truncate('result.json', 0, function() { })
+    fs.writeFile('result.json', JSON.stringify(menu), function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('OK!');
+        }
+    });
+});
+
+
---
+++ readme.md
@@ -0,0 +1,16 @@
+
+# Parsing [Sider](https://www.sider.biz/) menu data with Node.js.
+
+### Package:
+
+- [node-html-parser](https://www.npmjs.com/package/node-html-parser)
+
+### Running:
+- Make sure you have "./sider.html" file with live HTML.
+- Make sure you have empty "./result.json" file.
+- Run "node index.js".
+- Check "./result.json" file.
+
+### Author:
+
+- [BoolFalse](https://boolfalse.com)
